8666a034eecb0eff117868b7ae4b6c122e26041b
c376W <mailfzc123@gmail.com>
2019-03-06 15:31:58 -0800

    Adding new references.

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ class App extends Component {
     //this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons:[
-        {name: 'Max', age:28},
+        {name: 'Maximilian', age:28},
         {name: 'Manu', age:29},
-        {name: 'Stephanie', age:26}
+        {name: 'Stephanie', age:27}
         ] })
   }
 
@@ -30,7 +30,8 @@ class App extends Component {
         <h1>Hi, I'm a React App.</h1>
         <p>This is really working.</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age ={this.state.persons[0].age}>Ha</Person>
+        <Person name={this.state.persons[0].name} age ={this.state.persons[0].age} 
+          click = {this.switchNameHandler}>Ha</Person>
         <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}>Ha</Person>
         <Person name={this.state.persons[2].name} age ={this.state.persons[2].age}>Ha</Person>
       </div>
